f500c2f2363daba5175d2ae0b7f3c63c44a329a9
Dylan Matthews <dmmatth2@ncsu.edu>
2026-02-24 15:48:49 -0500

    Tried to fix featherweight.

--- data/abilities.ts
+++ data/abilities.ts
@@ -5896,11 +5896,9 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			return this.trunc(weighthg / 2);
 		},
         onDamage(damage, target, source, effect) {
-			if (effect.id === 'recoil') {
-				if (!this.activeMove) throw new Error("Battle.activeMove is null");
-				if (this.activeMove.id !== 'struggle') {
-                    this.boost({ spe: length }, source, source, this.dex.abilities.get('featherweight'));
-                    return;
+			if (effect?.id === 'recoil') {
+				if (this.activeMove?.id !== 'struggle') {
+                    this.boost({ spe: length }, target);
                 }
 			}
 		},
